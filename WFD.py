1fcebc9954ec385064a78264b005103424e19290
NPatton3467 <noah.patt3467@gmail.com>
2024-04-24 18:56:42 -0500

    Adding some helper functions. Haven't tested but this is the implementation as far as I'm aware

--- WFD.py
+++ WFD.py
@@ -2,9 +2,39 @@ import queue
 
 def is_frontier(node, occupancy_grid):
     # Evaluates whether a point is on the frontier of exploration
+    if occupancy_grid[node[0], node[1]] != 0.5:
+        return False # Assumes frontier must be unexplored
+    
+    for adj_node in get_adjacent(node):
+        if occupancy_grid[adj_node[0], adj_node[1]] == 0:
+            return True # Assumes frontier must be adjacent to free space
+        
+    return False # if none of the above must not be frontier
+
 
 def get_adjacent(node, occupancy_grid):
     # Get the adjacent nodes
+    adj_list = []
+    adj_dim_diffs = [-1, 0, 1]
+
+    for x_diff in adj_dim_diffs:
+        for y_diff in adj_dim_diffs:
+            # 0,0 skip since that is current node
+            if x_diff == 0 and y_diff == 0:
+                continue
+
+            n_x = node[0] + x_diff
+            n_y = node[1] + y_diff
+
+            if n_x > 0 and n_x < len(occupancy_grid):
+                assert False # This assumes 0 to max indexing for occupancy grid
+                if n_y > 0 and n_y < len(occupancy_grid[0]):
+                    assert False # This assumes that the occupancy grid is x by y 
+                    if occupancy_grid[nx, ny] == 1: 
+                        # Assumes 1 means obstacle
+                        adj_list.append((n_x, n_y))
+
+    return adj_dim_diffs
 
 def get_next_obs_point(occupancy_grid, pose):
     # Init map queue
